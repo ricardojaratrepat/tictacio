36e141becab57d77f39ea536dca72d7d88e39eed
Samuel <sialiaga@miuandes.cl>
2024-11-07 20:35:20 -0300

    *add register to logic

--- backend/database_services.py
+++ backend/database_services.py
@@ -4,3 +4,9 @@ def get_user(username:str) -> Dict[str, str]:
     if username == "juan":
         return {"username":"juan", "password":"$2b$12$i/lJ1oS5aovi.hfl/og/Z.tZOhm7do2IxEQgoNqs7bXyWGLko5HeW"}
     return None
+
+def add_user(username:str, password:str) -> Dict[str, str]:
+    if not get_user(username):
+        return {"username":username, "password":password}
+    else: 
+        None
--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from auth import validates_jwt, validates_login
 from jwt_manage import encodeJWT
+from database_services import add_user
+from hash_password import get_password_hash
 
-class LoginRequest(BaseModel):
+class LoginBody(BaseModel):
     username: str
     password: str
 
@@ -16,19 +18,9 @@ jwt_dependency = Annotated[dict, Depends(validates_jwt)]
 # FastAPI app
 app = FastAPI()
 
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5000/", "http://127.0.0.1:5173/"],  # Añade la URL de tu frontend aquí
-    allow_credentials=True,
-    allow_methods=[""],
-    allow_headers=[""],
-)
-
 # Ruta de login
 @app.post("/login")
-async def login(login_request: LoginRequest):
+async def login(login_request: LoginBody):
     user = validates_login(login_request.username, login_request.password)
     
     if not user:
@@ -41,6 +33,16 @@ async def login(login_request: LoginRequest):
     access_token = encodeJWT(login_request.username)
     return access_token
 
+@app.post("/register")
+async def register(login_request: LoginBody):
+    result = add_user(username=login_request.username, password=get_password_hash(login_request.password))
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already registered",
+        )
+    return {"message": "User registered successfully"}
+
 # Ruta protegida (requiere autenticación con Bearer token simple)
 @app.get("/protected-route")
 async def protected_route(jwt_data: jwt_dependency):
@@ -49,3 +51,11 @@ async def protected_route(jwt_data: jwt_dependency):
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED, detail="Not valid session"
     )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080/", "http://127.0.0.1:8080/"],  # Especifica los orígenes de tu frontend
+    allow_credentials=True,
+    allow_methods=[""],  # Permite todos los métodos HTTP
+    allow_headers=[""],  # Permite todos los encabezados necesarios
+)
