16e58c4d40563030e0dddd82d5fc160e5afb94ef
ricardojaratrepat <rajara1@miuandes.cl>
2024-11-07 21:22:57 -0300

    feat: changed url

--- frontend/src/components/Login.svelte
+++ frontend/src/components/Login.svelte
@@ -8,7 +8,7 @@
 
     async function handleLogin() {
         try {
-            const response = await fetch("http://backend:8000/login", {
+            const response = await fetch("http://localhost:8000/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
