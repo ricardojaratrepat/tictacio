308006c42052c0e1c97b93aa6b7c0ed0376c620a
ricardojaratrepat <rajara1@miuandes.cl>
2024-11-24 17:31:38 -0300

    fix: path in config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -8,7 +8,7 @@ export default {
       fallback: null,  // Archivo HTML que se usará en caso de rutas no encontradas
     }),
     paths: {
-      base: '/tictacio/', // Configuración adicional si tu proyecto usa un subdirectorio
+      base: '/tictacio', // Configuración adicional si tu proyecto usa un subdirectorio
     },
     prerender: {
       default: true, // Habilita la pre-renderización para generar contenido estático
