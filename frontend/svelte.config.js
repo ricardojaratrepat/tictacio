c27d5e504bca3e61fb83b4a427a3e00638ed954d
ricardojaratrepat <rajara1@miuandes.cl>
2024-11-24 17:25:34 -0300

    feat: svelte adapter

---
+++ frontend/svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+  kit: {
+    adapter: adapter({
+      pages: 'public',  // Carpeta donde se generan los archivos estáticos
+      assets: 'build', // Carpeta para los activos como CSS y JS
+      fallback: null,  // Archivo HTML que se usará en caso de rutas no encontradas
+    }),
+    paths: {
+      base: 'tictacio/', // Configuración adicional si tu proyecto usa un subdirectorio
+    },
+    prerender: {
+      default: true, // Habilita la pre-renderización para generar contenido estático
+    },
+  },
+};
